fa97304649f2652677baa1ee1fd80757621f4472
jact1001 <jact1001@gmail.com>
2022-08-26 11:23:09 -0500

    Add files via upload

--- server/routes/person.js
+++ server/routes/person.js
@@ -3,7 +3,7 @@ const recordRoutes = express.Router();
 const dbo = require("../db/conn");
 const ObjectId = require("mongodb").ObjectId;
 
-recordRoutes.route("/person/:id").get(function (req, res) {
+recordRoutes.route("/person-id/:id").get(function (req, res) {
     let db_connect = dbo.getDb();
     let myquery = { _id: ObjectId(req.params.id) };
     db_connect
@@ -14,72 +14,39 @@ recordRoutes.route("/person/:id").get(function (req, res) {
         });
 });
 
-recordRoutes.route("/record").get(function (req, res) {
-    let db_connect = dbo.getDb("employees");
-    db_connect
-        .collection("records")
-        .find({})
-        .toArray(function (err, result) {
-            if (err) throw err;
-            res.json(result);
-        });
-});
-
-// This section will help you get a single record by id
-recordRoutes.route("/record/:id").get(function (req, res) {
+recordRoutes.route("/person/:user").get(function (req, res) {
     let db_connect = dbo.getDb();
-    let myquery = { _id: ObjectId(req.params.id) };
+    let myquery = {username: {$eq: req.params.user}};
     db_connect
-        .collection("records")
+        .collection("persons")
         .findOne(myquery, function (err, result) {
             if (err) throw err;
             res.json(result);
         });
 });
 
-// This section will help you create a new record.
-recordRoutes.route("/record/add").post(function (req, response) {
+recordRoutes.route("/query-1").get(function (req, res) {
     let db_connect = dbo.getDb();
-    let myobj = {
-        name: req.body.name,
-        position: req.body.position,
-        level: req.body.level,
-    };
-    db_connect.collection("records").insertOne(myobj, function (err, res) {
-        if (err) throw err;
-        response.json(res);
-    });
-});
-
-// This section will help you update a record by id.
-recordRoutes.route("/update/:id").post(function (req, response) {
-    let db_connect = dbo.getDb();
-    let myquery = { _id: ObjectId(req.params.id) };
-    let newvalues = {
-        $set: {
-            name: req.body.name,
-            position: req.body.position,
-            level: req.body.level,
-        },
-    };
+    let myquery = { $and: [{ country: { $in: ["Colombia", "Panama"]}}, { $and: [ {register_date: { $gte: "2021-01-01"}}, {register_date: { $lt: "2022-01-01"}}] } ]};
     db_connect
-        .collection("records")
-        .updateOne(myquery, newvalues, function (err, res) {
+        .collection("persons")
+        .find(myquery)
+        .toArray(function (err, result) {
             if (err) throw err;
-            console.log("1 document updated");
-            response.json(res);
+            res.json(result);
         });
 });
 
-// This section will help you delete a record
-recordRoutes.route("/:id").delete((req, response) => {
+recordRoutes.route("/query-2").get(function (req, res) {
     let db_connect = dbo.getDb();
-    let myquery = { _id: ObjectId(req.params.id) };
-    db_connect.collection("records").deleteOne(myquery, function (err, obj) {
-        if (err) throw err;
-        console.log("1 document deleted");
-        response.json(obj);
-    });
+    let myquery = { $and: [ {register_date: { $gte: "2022-01-01"}},  {register_date: { $lt: "2023-01-01"}}, { answers: {$size: 0}}, {is_censored: false}] };
+    db_connect
+        .collection("question")
+        .find(myquery)
+        .toArray(function (err, result) {
+            if (err) throw err;
+            res.json(result);
+        });
 });
 
 module.exports = recordRoutes;
